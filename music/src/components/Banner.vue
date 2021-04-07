7c40498cc0343bb1cf51cc0837436f6c53861f42
lhm817 <1448687318@qq.com>
2021-04-07 17:22:41 +0800

    音乐项目部分内容

---
+++ music/src/components/Banner.vue
@@ -0,0 +1,52 @@
+<template>
+  <swiper ref="mySwiper" :options="swiperOptions">
+    <swiper-slide>Slide 1</swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+// import style (>= Swiper 6.x)
+import 'swiper/swiper-bundle.css'
+
+export default {
+  name: 'Banner',
+  data () {
+    return {
+      swiperOptions: {
+        loop: true, // 循环模式选项
+        autopaly: {
+          delay: 1000, // 自动切换时间间隔，单位ms
+          stopOnLastSlide: false, // 当切换到最后一个slide时停止自动切换
+          disableOnInteraction: false // 用户操作swiper之后，是否禁止autopaly
+        },
+        // 如果需要分页器
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true
+        // Some Swiper option/callback...
+      }
+    }
+  },
+  // props 除了可以用数组进行接收，也可以通过对象来接收
+  props: {
+    banners: {
+      type: Array,
+      default: () => [],
+      require: true
+    }
+  },
+  components: {
+    Swiper,
+    SwiperSlide
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- music/src/views/Recommend.vue
+++ music/src/views/Recommend.vue
@@ -1,12 +1,32 @@
 <template>
   <div class="recommend">
-    <p>热门推荐</p>
+    <Banner></Banner>
   </div>
 </template>
 
 <script>
+import { getBanner } from '../api/index'
+import Banner from '../components/Banner'
 export default {
-  name: 'Recommend'
+  name: 'Recommend',
+  comments: {
+    Banner
+  },
+  data: function () {
+    return {
+      banners: []
+    }
+  },
+  created () {
+    getBanner()
+      .then((data) => {
+        // console.log(data)
+        this.banners = data.banners
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
 }
 </script>
 
