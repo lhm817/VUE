cb1bc0c3d7e7ad4fa1c759a391359e7b2bfe5e3b
lhm817 <1448687318@qq.com>
2021-04-08 17:24:40 +0800

    推荐歌单、最新专辑

--- music/src/components/Banner.vue
+++ music/src/components/Banner.vue
@@ -1,25 +1,32 @@
 <template>
-  <swiper ref="mySwiper" :options="swiperOptions">
-    <swiper-slide>Slide 1</swiper-slide>
+  <!--swiper的bug,如果数据是从网络获取的, 那么自动轮播到最后一页之后就不轮播了-->
+  <!--只需要在swiper组件上面加上v-if="数据.length > 0"-->
+
+  <swiper ref="mySwiper" :options="swiperOption" class="banner" v-if="banners.length>0">
+    <swiper-slide v-for="value in banners" :key="value.bannerId" class="item">
+      <a :href="value.url">
+        <img :src="value.pic" alt="">
+      </a>
+    </swiper-slide>
     <div class="swiper-pagination" slot="pagination"></div>
   </swiper>
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-// import style (>= Swiper 6.x)
-import 'swiper/swiper-bundle.css'
+// import style (<= Swiper 5.x)
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 
 export default {
   name: 'Banner',
   data () {
     return {
-      swiperOptions: {
+      swiperOption: {
         loop: true, // 循环模式选项
-        autopaly: {
-          delay: 1000, // 自动切换时间间隔，单位ms
+        autoplay: {
+          delay: 1000, // 自动切换的时间间隔，单位ms
           stopOnLastSlide: false, // 当切换到最后一个slide时停止自动切换
-          disableOnInteraction: false // 用户操作swiper之后，是否禁止autopaly
+          disableOnInteraction: false // 用户操作swiper之后，是否禁止autoplay。
         },
         // 如果需要分页器
         pagination: {
@@ -28,25 +35,45 @@ export default {
         observer: true,
         observeParents: true,
         observeSlideChildren: true
-        // Some Swiper option/callback...
       }
     }
   },
-  // props 除了可以用数组进行接收，也可以通过对象来接收
   props: {
     banners: {
       type: Array,
       default: () => [],
-      require: true
+      required: true
     }
   },
   components: {
-    Swiper,
-    SwiperSlide
+    swiper,
+    swiperSlide
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .banner{
+    .item{
+      img{
+        width: 100%;
+        height: 300px;
+      }
+    }
+  }
+</style>
+<style lang="scss">
+@import "../assets/css/mixin";
+/*如果想要覆盖swiper的样式，那么style标签不能是scoped的，否则无法覆盖 */
+  .banner{
+    .swiper-pagination-bullet{
+      width: 16px;
+      height: 16px;
+      background: #fff;
+      opacity: 1;
+    }
+    .swiper-pagination-bullet-active{
+      @include bg_color();
+    }
+  }
 </style>
---
+++ music/src/components/SongList.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="song">
+  <div class="song-top">
+    <h3>最新音乐</h3>
+  </div>
+  <ul class="song-list">
+    <li></li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'SongList'
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/css/mixin";
+@import "../assets/css/variable";
+  .song{
+    width: 100%;
+    @include bg_sub_color;
+    .song-top{
+      width: 100%;
+      height: 84px;
+      line-height: 84px;
+      @include bg_sub_color();
+      padding: 0 20px;
+      border-bottom: 1px solid #ccc;
+      h3{
+        @include font_size($font_large);
+        @include font_color();
+        font-weight: bold;
+      }
+    }
+  }
+</style>
